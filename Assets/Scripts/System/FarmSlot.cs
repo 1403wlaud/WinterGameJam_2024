b2df80a8ff58b207032bc9d9c29927d3357dcad7
1403wlaud <jeemyung1129@gmail.com>
2024-12-17 16:13:28 +0900

    .

---
+++ Assets/Scripts/System/FarmSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmSlot : MonoBehaviour
+{
+    [HideInInspector] public Item curremtCrops;
+    public List<Item> items;
+    [HideInInspector] public int waterScore;
+    
+
+}
